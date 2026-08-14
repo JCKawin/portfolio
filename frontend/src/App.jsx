385d385af171c37a5a65c0955d27e8e5236fbb22
JCKawin <jckawin2007@gmail.com>
2026-08-14 16:21:58 +0530

    good done init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import portrait from '../public/VERT_original-girl-only-xhigh-bw.webp'
+import './App.css'
+
+function App() {
+  return (
+    <main className="stage">
+      <img src={portrait} alt="" className="portrait"  />
+    </main>
+  )
+}
+
+export default App
